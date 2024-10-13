1e256bcf6086be42e7b1dfb8a9c114df10a8f2ce
rohitrrr345 <rrajput2060@gmail.com>
2024-10-13 15:42:29 +0530

    aef

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -2,6 +2,7 @@
 export const isAuthenticated = (req, res, next) => {
     if (req.session && req.session.user) {
       req.userId = req.session.user._id; 
+      console.log(req.userId)
       next();
     } else {
       return res.status(401).json({ message: 'Unauthorized: Please log in' });
